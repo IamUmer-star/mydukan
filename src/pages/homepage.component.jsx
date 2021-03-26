655f36ade722a4f8d407c271872539ddef1b72e3
Qaisar.veeve.io <qaisar.mehmood@veeve.io>
2021-03-26 11:35:39 +0000

    dingdong

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './homepage.scss';
+import Directory from "../Components/directory/directory.component";
+
+
+const HomePage = () =>
+    (
+        <div className={'homepage'}>
+            <Directory/>
+        </div>
+    );
+
+
+export default HomePage;
