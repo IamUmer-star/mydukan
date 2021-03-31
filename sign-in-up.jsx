2881b845c473e1d3ea60c5da47002666bd1beca6
IamUmer-star <56833156+IamUmer-star@users.noreply.github.com>
2021-03-31 14:26:12 +0500

    Add files via upload

---
+++ sign-in-up.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './sign-in-up.scss';
+
+
+const SignInAndSingUpPage = ()=>{
+    return(
+
+        <div>
+            <h1>mango tango</h1>
+        </div>
+    )
+
+
+}
+
+export default SignInAndSingUpPage;
