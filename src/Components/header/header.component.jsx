969e9527d3dd3855ca10b85c8ea45d138c07a593
Qaisar.veeve <qaisar.mehmood@veeve.io>
2021-04-21 11:02:27 +0500

    addedd cart icon

--- src/Components/header/header.component.jsx
+++ src/Components/header/header.component.jsx
@@ -4,6 +4,7 @@ import {auth} from '../../firebase/firebase.utils';
 import {connect} from "react-redux";
 
 import {ReactComponent as Logo} from '../../assets/logo.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
 
 import './header.style.scss';
 
